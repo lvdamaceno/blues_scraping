2b54288222cc196a048a15ca16034129e38f86a7
lvdamaceno <lvdamaceno@gmail.com>
2018-11-18 19:51:08 -0300

    comentários do código

--- genius.py
+++ genius.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 from chaves import (CLIENT_ACCESS_TOKEN)
-import json
 
 '''
 Fonte
@@ -17,14 +16,23 @@ CLIENT_ACCESS_TOKEN = ''
 Informe seu token de acesso dentro das apas.
 '''
 
+# informaçãoes padrões que serão usadas no código
 defaults = {
     'request': {
+        # token de acesso à API do Genius
         'token': CLIENT_ACCESS_TOKEN,
         'base_url': 'https://api.genius.com'
     }}
 
 
 def request_song_info(song_title, artist_name):
+    '''
+    Essa função conecta via API ao site Genius e captura os dados referentes
+    à uma música com o seu cantor.
+    :param song_title: string, nome da musica à ser consultada
+    :param artist_name: string, nome do artista à ser consultado
+    :return: dicinário, contendo todos os dados da pesquisa
+    '''
     base_url = defaults['request']['base_url']
     headers = {'Authorization': 'Bearer ' + defaults['request']['token']}
     search_url = base_url + '/search'
@@ -34,6 +42,12 @@ def request_song_info(song_title, artist_name):
 
 
 def scrap_song_url(url):
+    '''
+    Essa função de posse da url capturada com a função request_song_info()
+    faz um scraping da página e retorna a letra completa de uma música.
+    :param url: string, url da música à ser consultada
+    :return: string, letra completa
+    '''
     page = requests.get(url)
     html = BeautifulSoup(page.text, 'html.parser')
     [h.extract() for h in html('script')]
--- main.py
+++ main.py
@@ -1,31 +1,50 @@
 from genius import *
 import json
-# para visualizar mais amigavelmente o dicionario que vem do site
-import pprint
 import time
 
 cantor = 'Gary Clark Jr.'
 
-# lista de musicas capturar à mão, proximo passo descobrir uma forma de
-# capturar todas aos nomes das musicas de uma cantor/compositor x
+'''
+Lista de musicas capturada de forma manual, proximo passo descobrir uma forma de
+capturar todas aos nomes das musicas de uma cantor/compositor.
+'''
+
+# abre o arquivo que contém a lista dos nomes das músicas
 arq = open('lista_musicas/gary_clarck_jr_music_list.txt', 'r')
+
+# salva a as musicas na varíavel lista_musica
 lista_musica = arq.readlines()
 
+'''
+Esse for entra na lista de musicas e captura as musicas via API do genius e salva cada uma
+em um arquivo separado, tendo o nome do arquivo montado como autor + titulo
+'''
 for musica in lista_musica:
+    # começa o laço e captura cada música uma a uma
+    # a variavel req recebe da API os dados completos sobre a música
     req = request_song_info(musica, cantor)
+    # dados_musica recebe o req convertido em dicionário
     dados_musica = json.loads(req.text)
+    # titulo da musica vindo da API
     titulo = dados_musica['response']['hits'][0]['result']['title']
+    # autor da musica vindo da API
     autor = dados_musica['response']['hits'][0]['result']['primary_artist']['name']
-    #  print(autor + ' - ' + titulo)
+    # url_musica recebe a url da musica no site do Genius para porde fazer o scrap
     url_musica = dados_musica['response']['hits'][0]['result']['url']
+    # letra_musica recebe apenas a letra da musica
     letra_musica = scrap_song_url(url_musica)
-    #  print(letra_musica)
+    # Confirmação na tela de que a música foi salva, criar aqui uma verificação melhor (try, except)
     print('Musica', titulo, 'salva!')
+    # monta o nome do arquivo para ser salvo
     nome_arquivo = autor + ' - ' + titulo
+    # cria o arquivo que será salvo contendo cada letra de musica separadamente
     arquivo = open('letras/' + str(nome_arquivo) + '.txt', 'w')
+    # salva a letra no .txt
     arquivo.writelines(letra_musica)
+    # fecha o arquivo
     arquivo.close()
-
+    # intervalo de 2 segundo entre uma chamada e outra da API, não sei se tem um limite ou não
     time.sleep(2)
 
+# Fecha o arquivo que contém os nomes das músicas
 arq.close()
